5c7a1cddb1428fde2e3646c52092ceef42b00e27
MateuszPestka <pes.matthew@gmail.com>
2022-03-11 09:19:04 +0000

    Set variable for js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,8 @@ let userScore = 0;
 let computerScore = 0;
 let userWin = document.getElementsByClassName("user-win");
 let computerWin = document.getElementsByClassName("computer-win");
+let scoreCount = document.getElementsByClassName("score-count");
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+
